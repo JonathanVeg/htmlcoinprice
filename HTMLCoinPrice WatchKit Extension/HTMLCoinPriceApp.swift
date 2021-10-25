b06c6eb450379c4a61ec5466255db8e6edee5a60
Jonathan Silva <jonathan.jgs@gmail.com>
2021-10-25 17:45:55 -0300

    Initial Commit

---
+++ HTMLCoinPrice WatchKit Extension/HTMLCoinPriceApp.swift
@@ -0,0 +1,19 @@
+//
+//  HTMLCoinPriceApp.swift
+//  HTMLCoinPrice WatchKit Extension
+//
+//  Created by Jonathan Goncalves da Silva on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HTMLCoinPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ HTMLCoinPrice/HTMLCoinPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HTMLCoinPriceApp.swift
+//  HTMLCoinPrice
+//
+//  Created by Jonathan Goncalves da Silva on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HTMLCoinPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
